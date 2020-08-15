042f17b55ded63cd59039c39831cdbbae80696af
justme <mrugankdevendradake@students.iisertirupati.ac.in>
2020-08-15 10:57:52 +0000

    just checking

--- multiclass_predictions_birds.py
+++ multiclass_predictions_birds.py
@@ -12,9 +12,6 @@ Multiclass classification problems using eco-acoustic features
 '''
 
 matplotlib.rcParams.update({'font.size': 24})
-#plt.rc('text', usetex=True)
-#matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
-#plt.rc('font', family='serif')
 
 feats = ['raw_audioset_feats_960ms']
 
@@ -29,28 +26,24 @@ subplt_idx = 1
 fig = plt.figure(figsize=(18,10))
 
 ax = plt.gca()
-#ax.text(-0.1, 1.15, 'check', transform=ax.transAxes,
-#fontsize=28, fontweight='bold', va='top', ha='right')
-
+Project_path = input('Project path: ')
 for f in feats:
-	# Load data from pickle files
-	with open(os.path.join('birds.pickle'), 'rb') as savef:
-		birds = pickle.load(savef)
-	birds = np.transpose(np.array(birds))
-	audio_feats_data, species, num_vecs = birds
-	BIRDS_LIST = []
-	for i in range(audio_feats_data.shape[0]):
-		toto = np.array(audio_feats_data[i], dtype = ('O')).astype(np.float)
-		BIRDS_LIST.append(toto)
-	BIRDS = np.array(BIRDS_LIST)
-	cm, cm_labs, average_acc, accuracies, cm_values = multi_class_classification(BIRDS, species, k_fold=k_folds)
-	#print(all_cms)
-	plot_multi_class_recalls(accuracies, cm_labs, average_acc, cm_values, 'species', f)
-	ax.set_title('Species classification')
-	#ax.set_ylabel('F1 score ($\%S)')
-	ax.set_xlabel("Bird species")
-	ax.set_ylabel("F1 score")
+        # Load data from pickle files
+        path_here = os.path.join(Project_path, 'Data/birds.pickle')
+        with open(path_here, 'rb') as savef:
+                birds = pickle.load(savef)
+        birds = np.transpose(np.array(birds))
+        audio_feats_data, species, num_vecs = birds
+        BIRDS_LIST = []
+        for i in range(audio_feats_data.shape[0]):
+                toto = np.array(audio_feats_data[i], dtype = ('O')).astype(np.float)
+                BIRDS_LIST.append(toto)
+        BIRDS = np.array(BIRDS_LIST)
+        cm, cm_labs, average_acc, accuracies, cm_values = multi_class_classification(BIRDS, species, k_fold=k_folds)
+        plot_multi_class_recalls(accuracies, cm_labs, average_acc, cm_values, 'species', f)
+        ax.set_title('Species classification')
+        ax.set_xlabel("Bird species")
+        ax.set_ylabel("F1 score")
     
 fig.savefig("birds_classification_with_metrics.png")
-#plt.tight_layout()
 plt.show()
