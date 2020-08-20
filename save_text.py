c86592dde94883630404d325730dab0d831a45b7
justme <mrugankdevendradake@students.iisertirupati.ac.in>
2020-08-20 18:44:31 +0000

    just checking

--- save_text.py
+++ save_text.py
@@ -1,13 +1,13 @@
 
 def make_annotation_file(save_path, day_label, time_stamp, species):
   text_file = open(save_path, 'w+')
-  text_file.write("Selection\t View\t	Channel\t	Begin Time (S)\t End Time (S)\t	Low Freq (Hz)\t	High Freq (Hz)\n")
+  text_file.write("Selection\t View	Channel	Begin Time (S)\t End Time (S)	Low Freq (Hz)	High Freq (Hz)\t Species\n")
   row_count = species.shape[0]
-  for row in row_count:
-    begin_time = 0.960 * row
-    end_time = begin_time + 0.960
+  for row in range(row_count):
+    begin_time = round(0.480 * row, 3)
+    end_time = round(begin_time + 0.480, 3)
     low_freq = 200
     high_freq = 8000
-    row_input = "{}\t Spectrogram 1\t 1\t {}\t {}\t {}\t {}\n".format(row, begin_time, end_time, low_freq, high_freq)
-    text_file.write("{row_input}")
+    row_input = "{}\t Spectrogram 1\t 1\t {}\t {}\t {}\t {}\t {}\n".format(row + 1, begin_time, end_time, low_freq, high_freq, species[row])
+    text_file.write(row_input)
   return text_file
