53967bff76c0e4f51828d0fbaa117657204ecc92
mrugank_colab <mrugankdevendradake@students.iisertirupati.ac.in>
2020-08-28 17:42:46 +0000

    changing annotation method

--- generate_ARU_annotations.py
+++ generate_ARU_annotations.py
@@ -15,8 +15,6 @@ Project_path = '/content/drive/My Drive/Sciurid Lab/CNN/VGGish_Birds/'
 path_here = os.path.join(Project_path, 'Data/birds_with_noise_single_notes_new.pickle')
 with open(path_here, 'rb') as savef:
   audio_feats_data_training, species_training, num_vecs = np.transpose(np.array(pickle.load(savef)))
-#birds = np.transpose(np.array(birds))
-# = birds
 BIRDS_LIST = []
 for i in range(audio_feats_data_training.shape[0]):
   toto = np.array(audio_feats_data_training[i], dtype = ('O')).astype(np.float)
@@ -31,7 +29,7 @@ clf.fit(BIRDS, species_training)
 # Load sound files for annotations
 for day in days:
   folder_name = Project_path + 'ARU_embeddings_no_overlap/' + day + '/'
-  file_names = os.listdir(folder_name)
+  file_names = sorted(os.listdir(folder_name))
   print(['We are on day ' + day])
 
   save_folder = Project_path + 'ARU_annotations_no_overlap_notes/'
@@ -44,6 +42,7 @@ for day in days:
   species_prediction_day = []
   num_preds_file = []
   duration = 0
+  
   # Make annotations
   for FILE in file_names:
     species_prediction = []
@@ -52,24 +51,20 @@ for day in days:
     with open(pickle_file_path, 'rb') as savef:
       wtf = pickle.load(savef)
     day_label, audio_feats_data, time_stamp = wtf['day'], wtf['raw_audioset_feats_960ms'], wtf['time_stamp']
-    #average_size = 5
-    #feat = np.zeros((audio_feats_data.shape[0] - average_size + 1, 128))
-    #for feat_count in range(feat.shape[0]):
-    #  for size in range(average_size):
-    #    feat[feat_count] += audio_feats_data[feat_count + size]
-    #  feat[feat_count] = feat[feat_count] / average_size
+    
     species_prediction.append(clf.predict(audio_feats_data))
     species_prediction_day.append(np.asarray(species_prediction))
     species_prediction = np.transpose(np.asarray(species_prediction))
     num_preds += species_prediction.shape[0]
     num_preds_file.append(num_preds)
     save_path = save_folder + day + '/' + time_stamp +  '.txt'
-    #make_annotation_file(save_path, species_prediction)
+    make_annotation_file(save_path, species_prediction)
     with contextlib.closing(wave.open(wav_file_path,'r')) as f:
       frames = f.getnframes()
       rate = f.getframerate()
     duration += frames / float(rate)
     duration_files.append(duration)
   species_prediction_day = np.asarray(species_prediction_day)
+  #print(species_prediction_day[0, 0, :])
   save_path_day = save_folder + day + '.txt'
   make_day_annotation_file(save_path_day, species_prediction_day, num_preds_file, duration_files)
--- save_text.py
+++ save_text.py
@@ -6,15 +6,30 @@ def make_annotation_file(save_path, species):
   row_count = species.shape[0]
   text_file = open(save_path, 'w+')
   text_file.write("Selection\t View	Channel	Begin Time (S)\t End Time (S)	Low Freq (Hz)	High Freq (Hz)\t Species\n")
-  for row in range(row_count):
+  annotation_count = 0
+  row = 0
+  while row < row_count:
     species_name = species[row][0]
+    skip = True
+    skip_annotation_count = 1
+    while skip == True:
+      if row + skip_annotation_count < row_count:
+        if species[row] ==  species[row + skip_annotation_count]:
+            skip_annotation_count += 1
+        else:
+          skip = False
+      else:
+        skip = False
+      
     if species_name != 'NOISE':
-      begin_time = round(0.960 * row, 3)
-      end_time = round(begin_time + 0.960, 3)
+      annotation_count += 1
+      begin_time = round(0.960 * row, 2)
+      end_time = round(begin_time + skip_annotation_count * 0.960, 2)
       low_freq = low_freq_dict[species_name]
       high_freq = high_freq_dict[species_name]
-      row_input = "{}\t Spectrogram 1\t 1\t {}\t {}\t {}\t {}\t {}\n".format(row + 1, begin_time, end_time, low_freq, high_freq, species_name)
+      row_input = "{}\t Spectrogram 1\t 1\t {}\t {}\t {}\t {}\t {}\n".format(annotation_count, begin_time, end_time, low_freq, high_freq, species_name)
       text_file.write(row_input)
+    row += skip_annotation_count
   return text_file
 
 def make_day_annotation_file(save_path, species, num_preds_file, duration_files):
@@ -28,24 +43,27 @@ def make_day_annotation_file(save_path, species, num_preds_file, duration_files)
     species_in_file = species[file_count, 0, :]
     column_count = species_in_file.shape[0]
     column = 0
-    while column <= column_count:
-      print(column)
+    while column < column_count:
       species_name = species_in_file[column]
       skip = True
       skip_annotation_count = 1
       while skip == True:
-        for skip_count in range(column + 1, column_count):
-          if species_in_file[column] ==  species_in_file[skip_count]:
-             skip_annotation_count += 1
+        if column + skip_annotation_count < column_count:
+          if species_in_file[column] ==  species_in_file[column + skip_annotation_count]:
+              skip_annotation_count += 1
           else:
             skip = False
-      column += skip_annotation_count
+        else:
+          skip = False
+      
       if species_name != 'NOISE':
         annotation_count += 1
         begin_time = round(0.960 * column + file_length, 2)
-        end_time = round(begin_time + (skip_annotation_count + 1) * 0.960, 2)
+        end_time = round(begin_time + skip_annotation_count * 0.960, 2)
         low_freq = low_freq_dict[species_name]
         high_freq = high_freq_dict[species_name]
         row_input = "{}\t Spectrogram 1\t 1\t {}\t {}\t {}\t {}\t {}\n".format(annotation_count, begin_time, end_time, low_freq, high_freq, species_name)
         text_file.write(row_input)
+
+      column += skip_annotation_count
   return text_file
