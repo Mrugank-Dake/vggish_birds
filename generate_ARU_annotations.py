0569746baded1959d074dc3710d4ae4e8cb407c8
mrugank_colab <mrugankdevendradake@students.iisertirupati.ac.in>
2020-08-28 08:15:02 +0000

    one file for day

--- generate_ARU_annotations.py
+++ generate_ARU_annotations.py
@@ -3,23 +3,24 @@ import numpy as np
 import os
 import random
 from sklearn.ensemble import RandomForestClassifier
-from save_text import make_annotation_file
+from save_text import make_annotation_file, make_day_annotation_file
 
 days = ['02', '04']
-concatenated_pickle = []
-Project_path = input('Project path: ')
+Project_path = '/content/drive/My Drive/Sciurid Lab/CNN/VGGish_Birds/'
+#/input('Project path: ')
 
 # Load training data from pickle files
 path_here = os.path.join(Project_path, 'Data/birds_with_noise_single_notes_new.pickle')
 with open(path_here, 'rb') as savef:
-  birds = pickle.load(savef)
-birds = np.transpose(np.array(birds))
-audio_feats_data_training, species_training, num_vecs = birds
+  audio_feats_data_training, species_training, num_vecs = np.transpose(np.array(pickle.load(savef)))
+#birds = np.transpose(np.array(birds))
+# = birds
 BIRDS_LIST = []
 for i in range(audio_feats_data_training.shape[0]):
   toto = np.array(audio_feats_data_training[i], dtype = ('O')).astype(np.float)
   BIRDS_LIST.append(toto)
 BIRDS = np.array(BIRDS_LIST)
+#for count_train in range(len(species_trainin))
 
 # Train classifier
 clf = RandomForestClassifier(random_state=0, n_estimators=100)
@@ -29,15 +30,20 @@ clf.fit(BIRDS, species_training)
 for day in days:
   folder_name = Project_path + 'ARU_embeddings_no_overlap/' + day + '/'
   file_names = os.listdir(folder_name)
-  print(['We are on day ' + day]) 
-  save_folder = Project_path + 'ARU_annotations_no_overlap_notes/' + day + '/'
+  print(['We are on day ' + day])
+
+  save_folder = Project_path + 'ARU_annotations_no_overlap_notes/'
   if not os.path.exists(save_folder):
     os.mkdir(save_folder)
   
+  duration_files = [0]
+  species_prediction_day = []
+  duration = 0
   # Make annotations
   for FILE in file_names:
     species_prediction = []
-    with open(os.path.join(folder_name, FILE), 'rb') as savef:
+    file_path = os.path.join(folder_name, FILE)
+    with open(file_path, 'rb') as savef:
       wtf = pickle.load(savef)
     day_label, audio_feats_data, time_stamp = wtf['day'], wtf['raw_audioset_feats_960ms'], wtf['time_stamp']
     #average_size = 5
@@ -47,6 +53,14 @@ for day in days:
     #    feat[feat_count] += audio_feats_data[feat_count + size]
     #  feat[feat_count] = feat[feat_count] / average_size
     species_prediction.append(clf.predict(audio_feats_data))
+    species_prediction_day.append(clf.predict(audio_feats_data))
     species_prediction = np.transpose(np.asarray(species_prediction))
-    save_path = save_folder + time_stamp +  '.txt'
-    make_annotation_file(save_path, day_label, time_stamp, species_prediction)
+    save_path = save_folder + day + '/' + time_stamp +  '.txt'
+    make_annotation_file(save_path, species_prediction)
+    with contextlib.closing(wave.open(file_path,'r')) as f:
+      frames = f.getnframes()
+      rate = f.getframerate()
+    duration += frames / float(rate)
+    duration_files.append(duration)
+  save_path_day = save_folder + '.txt'
+  make_day_annotation_file(save_path_day, species_prediction, duration_files)
--- save_text.py
+++ save_text.py
@@ -1,36 +1,34 @@
 
-def make_annotation_file(save_path, day_label, time_stamp, species):
+def make_annotation_file(save_path, species):
+  low_freq_dict = {'FINI': 3000, 'CUCE': 2500, 'MOFA': 500, 'POHO': 500, 'PHMA': 3000, 'GASO': 500, 'HYGA': 2000}
+  high_freq_dict = {'FINI': 8000, 'CUCE': 6000, 'MOFA': 3500, 'POHO': 2500, 'PHMA': 7000, 'GASO': 2000, 'HYGA': 6500}
+  row_count = species.shape[0]
   text_file = open(save_path, 'w+')
   text_file.write("Selection\t View	Channel	Begin Time (S)\t End Time (S)	Low Freq (Hz)	High Freq (Hz)\t Species\n")
-  row_count = species.shape[0]
   for row in range(row_count):
-    if species[row][0] != 'NOISE':
+    species_name = species[row][0]
+    if species_name != 'NOISE':
       begin_time = round(0.960 * row, 3)
       end_time = round(begin_time + 0.960, 3)
-      if species[row][0] == 'FINI':
-        low_freq = 3000
-        high_freq = 8000
-      elif species[row][0] == 'CUCE':
-        low_freq = 2500
-        high_freq = 6000
-      elif species[row][0] == 'MOFA':
-        low_freq = 500
-        high_freq = 3500
-      elif species[row][0] == 'POHO':
-        low_freq = 500
-        high_freq = 2500
-      elif species[row][0] == 'PHMA':
-        low_freq = 3000
-        high_freq = 7000
-      elif species[row][0] == 'GASO':
-        low_freq = 500
-        high_freq = 2000
-      elif species[row][0] == 'HYGA':
-        low_freq = 2000
-        high_freq = 6500
-      else:
-        low_freq = 200
-        high_freq = 8000
-      row_input = "{}\t Spectrogram 1\t 1\t {}\t {}\t {}\t {}\t {}\n".format(row + 1, begin_time, end_time, low_freq, high_freq, species[row][0])
+      low_freq = low_freq_dict[species_name]
+      high_freq = high_freq_dict[species_name]
+      row_input = "{}\t Spectrogram 1\t 1\t {}\t {}\t {}\t {}\t {}\n".format(row + 1, begin_time, end_time, low_freq, high_freq, species_name)
+      text_file.write(row_input)
+  return text_file
+
+def make_day_annotation_file(save_path, species, duration):
+  low_freq_dict = {'FINI': 3000, 'CUCE': 2500, 'MOFA': 500, 'POHO': 500, 'PHMA': 3000, 'GASO': 500, 'HYGA': 2000}
+  high_freq_dict = {'FINI': 8000, 'CUCE': 6000, 'MOFA': 3500, 'POHO': 2500, 'PHMA': 7000, 'GASO': 2000, 'HYGA': 6500}
+  row_count = species.shape[0]
+  text_file = open(save_path, 'w+')
+  text_file.write("Selection\t View	Channel	Begin Time (S)\t End Time (S)	Low Freq (Hz)	High Freq (Hz)\t Species\n")
+  for row in range(row_count):
+    species_name = species[row][0]
+    if species_name != 'NOISE':
+      begin_time = round(0.960 * row, 3)
+      end_time = round(begin_time + 0.960, 3)
+      low_freq = low_freq_dict[species_name]
+      high_freq = high_freq_dict[species_name]
+      row_input = "{}\t Spectrogram 1\t 1\t {}\t {}\t {}\t {}\t {}\n".format(row + 1, begin_time, end_time, low_freq, high_freq, species_name)
       text_file.write(row_input)
   return text_file
